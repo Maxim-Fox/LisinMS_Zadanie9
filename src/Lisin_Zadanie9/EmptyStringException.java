732baca504f65f7fd455d0a86b6a3497fa214422
Maxim-Fox <maxim_lisin@mail.ru>
2020-12-16 17:47:05 +0300

    Initial commit

---
+++ src/Lisin_Zadanie9/EmptyStringException.java
@@ -0,0 +1,8 @@
+package Lisin_Zadanie9;
+
+public class EmptyStringException extends Exception {
+
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
---
+++ src/Lisin_Zadanie9/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package Lisin_Zadanie9;
+
+public class StudentNotFoundException extends Exception {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
